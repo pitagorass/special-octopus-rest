2ea6fed5eba0709c2430ee7ffc3ce529df6b163d
Santiago <ECM9305H@comcel.com.co>
2021-05-04 11:11:35 -0500

    Initial structure

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
-
 // capturar body
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 // Conexión a Base de datos
-
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.iavkh.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+const optional = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
 // import routes
 const authRoutes = require('./src/routes/auth')
-//route middelware
+const verifyToken = require('./src/middleware/validate-token');
+const dashboadRoutes = require('./src/routes/dashboard');
+
+// route middlewares
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
 app.use('/api/user',authRoutes)
-
 // iniciar server
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`servidor andando en: ${PORT}`)
 })
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,18 +1,86 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const Joi = require('@hapi/joi');
+// Constraseña
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/login', async (req, res) => {
+    // validaciones
+    const { error } = schemaLogin.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message })
+    
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json({ error: 'Usuario no encontrado' });
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).json({ error: 'contraseña no válida' })
+    // create token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, process.env.TOKEN_SECRET)
+
+    res.header('auth-token', token).json({
+        error: null,
+        data: {token}
+    })
+})
+
+
+router.post('/register', async (req, res) => {
+
+    // validate user
+    const { error } = schemaRegister.validate(req.body)
+    if (error) {
+        return res.status(400).json(
+            { error: error.details[0].message }
+        )
+    }
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist) {
+        return res.status(400).json(
+            { error: 'Email ya registrado' }
+        )
+    }
+     // hash contraseña
+     const salt = await bcrypt.genSalt(10);
+     const password = await bcrypt.hash(req.body.password, salt);
+ 
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: password
+    });
+    try {
+        const savedUser = await user.save();
+        res.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+})
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
 
 router.get('/', (req, res) => {
     console.log('Ingresamos en el metodo get inicial, Bienvenido')
     res.json({
         estado: true,
         mensaje: 'funciona!'
-    })
-})
-router.post('/register', async (req, res) => {
-    console.log('Ingresamos en el metodo post para realizar el registro')
-    res.json({
-        error: null,
-        data: 'aquí va ir la data'
-    })
+    });
+
 })
 
 module.exports = router;
